0875006bc03f416a000651257dca52cfef046a00
Pierre-Jean Pollot <pierre-jean.pollot@eleves.ec-nantes.fr>
2022-02-16 15:43:23 +0900

    log-lik gradient (need to be tested)

--- gp/models.py
+++ gp/models.py
@@ -1,6 +1,6 @@
 import numpy as np
 from numpy import pi, log, sqrt
-from numpy.linalg import solve, cholesky
+from numpy.linalg import solve, cholesky, inv
 from scipy import rand
 from scipy.stats import multivariate_normal
 
@@ -10,12 +10,18 @@ class GPRegressor:
     input_dim: the input dimension
     kernel_function: the kernel function
     noise: the noise parameter (sigma_n) in the regression
+    epsilon: correction hyperparameter to avoid singular covariance matrix
     """
-    def __init__(self, input_dim, kernel_function, noise=1e-10):
+    def __init__(self, input_dim, kernel_function, noise=1e-10, epsilon=1e-10):
         self._d = input_dim
         self._kernel = kernel_function
         self._param = kernel_function._param
         self._param["noise"] = noise
+        self._eps = epsilon
+
+        self._grad = dict()
+        for key in self._param:
+            self._grad[key] = None        
 
         self._n = 0
         self._X = None
@@ -23,23 +29,34 @@ class GPRegressor:
         self._chol = None
         self._alpha = None
         self._loglik = None
-        self._grad = None
 
     """
-    TODO: Calculate the gradient of the log likelihood
+    epsilon: adjusting factor, make sure the covariance will be invertible
     """
     def fit(self, X, y):
         self._n = len(X)
         self._X = X
         self._y = y
         
-        # Calculate the matrix K + sigma_n * I
+        # Calculate the matrix K + sigma_n * I and its gradient
         M = np.zeros((self._n, self._n))
+        grad_M = dict()
+        for key in self._param:
+            grad_M[key] = np.zeros((self._n,self._n))
+
         for i in range(self._n):
-            for j in range(i):
-                M[i,j] = self._kernel.evaluate(X[i,:],X[j,:])
+            for j in range(i+1):
+                M[i,j], g = self._kernel.evaluate(X[i,:],X[j,:], return_grad=True)
                 M[j,i] = M[i,j]
-            M[i,i] = self._kernel.evaluate(X[i,:],X[i,:]) + self._param['noise']**2
+                if i==j:
+                    M[i,i] += self._param["noise"]**2 + self._eps
+                    g["noise"] = 2*self._param["noise"]
+                else:
+                    g["noise"] = 0
+                
+                for key in self._param:
+                    grad_M[key][i,j] = g[key]
+            
         ## Return its Cholesky matrix
         self._chol = cholesky(M)
         
@@ -47,11 +64,25 @@ class GPRegressor:
         z = solve(self._chol, y)
         self._alpha = solve(self._chol.T, z)
 
-        # Calculate the log likelihood
+        # Calculate the log-likelihood
         self._loglik = -(y.T @ self._alpha)/2 - self._n * log(2*pi)/2
         for i in range(self._n):
             self._loglik -= log(self._chol[i,i])
 
+        # Calculate the gradient of the log-likelihood
+        inv_M = inv(M)
+
+        A = np.zeros((self._n, self._n))
+        for i in range(self._n):
+            for j in range(i+1):
+                A[i,j] = self._alpha[i]*self._alpha[j]
+                A[j,i] = A[i,j]
+
+        for key in self._param:
+            Q = (A - inv_M) @ grad_M[key]
+            self._grad[key] = np.trace(Q)/2
+        
+        return self
     
     """
     return_cov: return the covariance over the predictions
@@ -87,11 +118,3 @@ class GPRegressor:
                 return f, sqrt(np.diag(cov))
         else:
             return f
-        
-    """
-    epsilon: positive adjustement factor for the covariance matrix to cope with singularity
-    """
-    def sample(self, X, size=1, epsilon=1e-10, random_state=None):
-        f, cov = self.predict(X, return_cov=True)
-        K = cov + epsilon*np.identity(len(X))
-        return multivariate_normal(mean=f, cov=K).rvs(size=size, random_state=random_state)
